912c592b1b5374b7aacee75e1b056049368a638e
Jimmy Tuck <jim.tuck15@gmail.com>
2018-10-05 22:48:38 +0000

    Fixed Bug on repos being removed when search box is empty

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -33,14 +33,14 @@ function repoInformationHTML (repos) {
                         <strong>Repo List:</strong>
                     </p>
                     <ul>
-                        ${listItemsHTML.join("/n")}
+                        ${listItemsHTML.join("\n")}
                     </ul>
                 </div>`;
 }
 
 function fetchGitHubInformation(event) {
-    $("gh-user-data").html("");
-    $("gh-repo-data").html("");
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     
     var username = $("#gh-username").val();
     if (!username) {
@@ -67,6 +67,9 @@ function fetchGitHubInformation(event) {
             function (errorResponse) {
                 if (errorResponse.status === 404) {
                     $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                } else if (errorResponse.status === 403) {
+                    var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                    $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
                 } else {
                     console.log(errorResponse);
                     $("#gh-user-data").html(
