473a940da62f575490da4d0a04c28d5db4f65db7
Jimmy Tuck <jim.tuck15@gmail.com>
2018-10-05 22:29:06 +0000

    Fixed bugs in gh-user-data and gh-repo-data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -39,6 +39,8 @@ function repoInformationHTML (repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("gh-user-data").html("");
+    $("gh-repo-data").html("");
     
     var username = $("#gh-username").val();
     if (!username) {
@@ -60,7 +62,9 @@ function fetchGitHubInformation(event) {
                 var repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHTML(userData)); 
                 $("#gh-repo-data").html(repoInformationHTML(repoData)); 
-            }, function (errorResponse) {
+            }, 
+            
+            function (errorResponse) {
                 if (errorResponse.status === 404) {
                     $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
                 } else {
@@ -70,3 +74,5 @@ function fetchGitHubInformation(event) {
                 }
             });
 }
+
+$(document).ready(fetchGitHubInformation);
